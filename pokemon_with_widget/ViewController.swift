3464234fb80e5505f94dd84a4b953b2d575baa91
Alif Ramadhoni <annur.alif@phincon.com>
2022-10-17 10:39:45 +0700

    Initial Commit

---
+++ pokemon_with_widget/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  pokemon_with_widget
+//
+//  Created by Alif Phincon on 17/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ pokemon_with_widgetUITests/pokemon_with_widgetUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  pokemon_with_widgetUITestsLaunchTests.swift
+//  pokemon_with_widgetUITests
+//
+//  Created by Alif Phincon on 17/10/22.
+//
+
+import XCTest
+
+class pokemon_with_widgetUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
